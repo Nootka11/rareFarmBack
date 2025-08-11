49c722f6dbff8697740c4593d991e351bf6ac264
Nootka11 <emanuelapetri@gmail.com>
2025-08-11 17:11:44 +0200

    modif en server.js

--- server.js
+++ server.js
@@ -22,8 +22,10 @@ require('dotenv').config();
 
 connectDB(process.env.MONGODB_URI || 'mongodb://localhost:27017/RareFarm');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000; 
 
 const server = createServer(app);
 
